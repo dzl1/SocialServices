e0f701526213257c426c1a40f8160f77ba8f0a67
Dave Lasike <dave.lasike@live.com>
2020-01-28 07:26:33 +1300

    initial commit

---
+++ HazeRE/HazeRE/HazeRE/HazeRE/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using HazeRE.Services;
+using HazeRE.Views;
+
+namespace HazeRE
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
